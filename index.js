738d12aa2fdf4f401c78660102ac93ec9165023e
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-11-24 03:36:08 +0600

    server setup done and connected with mongodb

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { query } = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.auieprw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productsCollection = client.db("mobileBechi").collection("products");
+
+    app.get("/apple-products", async (req, res) => {
+      const query = {Categorey:"apple"};
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Mobile Bechi Server Running");
+});
+
+app.listen(port, () => console.log(`Mobile Bechi Server Running on ${port}`));
